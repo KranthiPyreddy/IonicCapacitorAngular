1f14dd288a4c7429f4c278ad886d6e1c7d8aaf2d
kranthi pyreddy <kra330@gmail.com>
2020-12-04 14:01:11 -0500

    PartB-LAB5 Introduction to IonicCapacitorAngular

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+email: string = "";
+password: string = "";
+  signIn() {
+    alert("You entered the email: " + this.email + ", and the password: " + this.password);
+    }
   constructor() {}
 
 }
